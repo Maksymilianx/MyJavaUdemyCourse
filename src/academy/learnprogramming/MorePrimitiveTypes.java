39122076ed149f965393065bcebc55b591c4c6fd
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-10-19 13:30:16 +0200

    add boolean basics

--- src/academy/learnprogramming/MorePrimitiveTypes.java
+++ src/academy/learnprogramming/MorePrimitiveTypes.java
@@ -13,5 +13,11 @@ public class MorePrimitiveTypes {
         System.out.println(myUnicodeChar);
         char myCopyrightChar = '\u00A9';
         System.out.println(myCopyrightChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        // in Java terms we have boolean primitive type, and it can be set to true or false only.
+        boolean isCustomerOverTwentyOne = true;
     }
 }
