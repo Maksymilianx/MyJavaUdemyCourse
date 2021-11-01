3b4900315649bbeee8595e6dc8a9834111001837
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-11-01 15:30:55 +0100

    add primitive types recap

--- src/academy/learnprogramming/MorePrimitiveTypes.java
+++ src/academy/learnprogramming/MorePrimitiveTypes.java
@@ -10,14 +10,25 @@ public class MorePrimitiveTypes {
         // unicode-table.com
         char myUnicodeChar = '\u0044';
         System.out.println(myChar);
-        System.out.println(myUnicodeChar);
+        System.out.println("My Unicode character is " + myUnicodeChar);
         char myCopyrightChar = '\u00A9';
-        System.out.println(myCopyrightChar);
+        System.out.println("Copyright Character is " + myCopyrightChar);
 
         boolean myTrueBooleanValue = true;
         boolean myFalseBooleanValue = false;
 
         // in Java terms we have boolean primitive type, and it can be set to true or false only.
         boolean isCustomerOverTwentyOne = true;
+
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("Last string is equal to " + lastString);
+        // Java do not raise syntax error like Python when we try to add int to str, Java converts int to str, in order
+        // to add two values.
+        int another = 50;
+        double next = 50.50d;
+        another = (int) (another + next);
+        System.out.println("Add double with int = " + another );
     }
 }
