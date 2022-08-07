576b48f5e5baeb43d28707e3a87ce3f4fd552ec2
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2022-08-07 15:07:28 +0200

    add megaBytes converter

---
+++ src/sectionNr4/MegaBytesConverter.java
@@ -0,0 +1,17 @@
+package sectionNr4;
+
+public class MegaBytesConverter {
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        int Mb = kiloBytes / 1024;
+        int moduloMb = kiloBytes % 1024;
+        if (kiloBytes < 0) System.out.println("Invalid Value");
+        else {
+            System.out.println(kiloBytes + " KB = " + Mb + " MB and " + moduloMb + " KB");
+        }
+    }
+
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(-2500);
+    }
+}
