499b981e31e9134719f58f7f47f1070777b2ca33
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2022-08-07 11:20:52 +0200

    add speed converter methods

---
+++ src/sectionNr4/SpeedConverter.java
@@ -0,0 +1,23 @@
+package sectionNr4;
+
+public class SpeedConverter {
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) return -1;
+        return Math.round(kilometersPerHour / 1.609);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        if (kilometersPerHour < 0) System.out.println("Invalid value");
+        else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + "km/h= " + milesPerHour + "mi/h");
+        }
+    }
+
+    public static void main(String[] args) {
+        // if function does not print line in return, we have to do it later in void main function
+        System.out.println("Miles = " + toMilesPerHour(10.5));
+        printConversion(10.5);
+    }
+}
