91309c29ac76fd4ceb70defe28d44469815fa443
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2022-08-07 10:15:56 +0200

    add simple exercise

---
+++ src/sectionNr4/positiveNegative.java
@@ -0,0 +1,18 @@
+package sectionNr4;
+
+public class positiveNegative {
+
+    public static void checkNumber(int number) {
+        if (number > 0)
+            System.out.println("possitive");
+        else if (number < 0)
+            System.out.println("negative");
+        else System.out.println("zero number");
+    }
+
+    public static void main (String[] someArgs) {
+        checkNumber(10);
+        checkNumber(-10);
+        checkNumber(0);
+    }
+}
