f72d234a6f03389647fd695281028929838fbfea
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2022-08-10 10:12:14 +0200

    add simple exercise

---
+++ src/sectionNr4/EqualSumChecker.java
@@ -0,0 +1,11 @@
+package sectionNr4;
+
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int x, int y, int z) {
+        return (x + y == z);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum(1,-1,0));
+    }
+}
