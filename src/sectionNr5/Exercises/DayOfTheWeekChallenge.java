3a2d23feaaded5b1ea28e89c5703433cf65d39d7
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2022-08-18 19:10:13 +0200

    add switch exercise

--- src/sectionNr5/Exercises/DayOfTheWeekChallenge.java
+++ src/sectionNr5/Exercises/DayOfTheWeekChallenge.java
@@ -1,2 +1,57 @@
-package sectionNr5.Exercises;public class DayOfTheWeekChallenge {
+package sectionNr5.Exercises;
+
+public class DayOfTheWeekChallenge {
+    public static void printDayOfTheWeek(int day) {
+        switch (day) {
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
+        }
+    }
+
+    public static void stringValue (int day) {
+        if (day == 0) {
+            System.out.println("Sunday");
+        } else if (day == 1) {
+            System.out.println("Monday");
+        } else if (day == 2) {
+            System.out.println("Tuesday");
+        } else if (day == 3) {
+            System.out.println("Wednesday");
+        } else if (day == 4) {
+            System.out.println("Thursday");
+        } else if (day == 5) {
+            System.out.println("Friday");
+        } else if (day == 6) {
+            System.out.println("Saturday");
+        } else {
+            System.out.println("Invalid day");
+        }
+    }
+
+    public static void main(String[] args) {
+        printDayOfTheWeek(8);
+        stringValue(8);
+    }
 }
