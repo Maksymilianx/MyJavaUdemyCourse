377acd243e4603bf9d6dc3e188a7ed9b92f096b2
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2022-08-20 12:16:49 +0200

    add while lesson with exercise

--- src/sectionNr5/Lessons/DigitSum.java
+++ src/sectionNr5/Lessons/DigitSum.java
@@ -1,2 +1,29 @@
-package sectionNr5.Lessons;public class DigitSum {
+package sectionNr5.Lessons;
+
+public class DigitSum {
+
+    public static void main(String[] args) {
+        System.out.println("Sum of digits in number 125 is " + sumDigits(125));
+        System.out.println("Sum of digits in number -125 is " + sumDigits(-125));
+        System.out.println("Sum of digits in number 4 is " + sumDigits(4));
+        System.out.println("Sum of digits in number 32123 is " + sumDigits(32123));
+    }
+
+    private static int sumDigits(int number) {
+        if (number < 10) {
+            return -1;
+        }
+        int sum = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            sum += digit;
+
+            // drop the least-significant digit
+            number /= 10;   // same as number = number / 10;
+        }
+        return sum;
+    }
 }
+
+
+
