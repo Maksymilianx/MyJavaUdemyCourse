e55d2955d91fe5eb5d60efe926addfd9b12874ea
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2022-08-21 11:05:44 +0200

    add unfinished exercise

---
+++ src/sectionNr5/Exercises/GreatestCommonDivisor.java
@@ -0,0 +1,15 @@
+package sectionNr5.Exercises;
+
+public class GreatestCommonDivisor {
+
+    public static int getGreatestCommonDivisor (int first, int second) {
+        if ((first < 0) || (second < 0)) {
+            return -1;
+        }
+        int current = 0;
+        int prev = 0;
+        for (int i = 1; i == first; i++) {
+
+        }
+    }
+}
