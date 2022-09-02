b9927acd12cede2770254bec9b81f6c65731bf4a
Maksymilian Krajewski/Modem Verification Support II (MDV) /SRPOL/Professional/Samsung Electronics <m.krajewski2@samsung.com>
2022-09-02 14:33:32 +0200

    add unfinished lesson

---
+++ src/sectionNr5/Lessons/ForStatements.java
@@ -0,0 +1,40 @@
+package sectionNr5.Lessons;
+
+public class ForStatements {
+
+    public static void main(String[] args) {
+//        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+
+        for (int i=2; i<9; i++) {
+            System.out.println("10,000 at " + i + "% interest = " +
+                    String.format("%.2f", calculateInterest(10000.0, i)));
+        }
+       System.out.println("#####################");
+        for (int i=8; i>1; i--) {
+            System.out.println("10.000 at " + i + "% interest = " +
+                    String.format("%.2f", calculateInterest(10000.0, i)));
+        }
+        System.out.println("#####################");
+
+        for (int )
+    }
+
+    public static boolean isPrime (int n) {
+
+        if (n == 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= n/2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static double calculateInterest(double ammount, double interestRate) {
+        return (ammount * (interestRate / 100));
+    }
+}
