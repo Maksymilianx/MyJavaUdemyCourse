08d3e106fd5afc6aacdda43ea7bcbc9ee410a827
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2022-09-03 17:59:42 +0200

    add exercise

---
+++ src/sectionNr5/Exercises/FactorPrinter.java
@@ -0,0 +1,13 @@
+package sectionNr5.Exercises;
+
+public class FactorPrinter {
+    public static int printFactors(int num) {
+        if (num < 1) {
+            System.out.println("Invalid Value");
+        }
+        else {
+            for (int i=num; i>1; i++)
+        }
+
+    }
+}
--- src/sectionNr5/Exercises/GreatestCommonDivisor.java
+++ src/sectionNr5/Exercises/GreatestCommonDivisor.java
@@ -3,13 +3,24 @@ package sectionNr5.Exercises;
 public class GreatestCommonDivisor {
 
     public static int getGreatestCommonDivisor (int first, int second) {
-        if ((first < 0) || (second < 0)) {
+
+        if (first < 10 || second < 10) {
             return -1;
         }
-        int current = 0;
-        int prev = 0;
-        for (int i = 1; i == first; i++) {
 
+        while (first != second) {
+            if (first > second) {
+                first = first - second;
+            }
+            else {
+                second = second - first;
+            }
         }
+        return second;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(getGreatestCommonDivisor(25, 15));
+        System.out.println(getGreatestCommonDivisor(81, 14));
     }
 }
