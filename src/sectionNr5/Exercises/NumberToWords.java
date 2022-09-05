bed5c0b4ffd08030c0cffceff90c761ccc4cd60a
Maksymilian Krajewski/Modem Verification Support II (MDV) /SRPOL/Professional/Samsung Electronics <m.krajewski2@samsung.com>
2022-09-05 15:11:58 +0200

    add unfinished Numbers To Worlds exercise

---
+++ src/sectionNr5/Exercises/NumberToWords.java
@@ -0,0 +1,46 @@
+package sectionNr5.Exercises;
+
+public class NumberToWords {
+
+    public static void numberToWords(int number) {
+
+        if (number < 1) System.out.println("Invalid Value");
+        else {
+            int remaining = number % 10;
+            switch (remaining) {
+                case 0:
+                    System.out.println("Zero");
+                    break;
+                case 1:
+                    System.out.println("One");
+                    break;
+                case 2:
+                    System.out.println("Two");
+                    break;
+                case 3:
+                    System.out.println("Three");
+                    break;
+                case 4:
+                    System.out.println("Four");
+                    break;
+                case 5:
+                    System.out.println("Five");
+                    break;
+                case 6:
+                    System.out.println("Six");
+                    break;
+                case 7:
+                    System.out.println("Seven");
+                    break;
+                case 8:
+                    System.out.println("Eight");
+                    break;
+                case 9:
+                    System.out.println("Nine");
+                    break;
+            }
+            int withoutLastNr = number - remaining;
+
+        }
+    }
+}
