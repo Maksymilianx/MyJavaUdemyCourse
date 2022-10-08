097139588d5cf027e1a1b2a54f80c0202478a4d8
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2022-10-09 00:05:51 +0200

    add exercise

--- src/sectionNr5/Exercises/NumberToWords.java
+++ src/sectionNr5/Exercises/NumberToWords.java
@@ -80,7 +80,7 @@ public class NumberToWords {
         if (number<0){return -1;}
         if (number==0){return 1;}
         int digitCount=0;
-        for (int i =0;number>0;i++){
+        for (int i=0;number>0;i++){
             number/=10;
             digitCount++;
         }return digitCount;
