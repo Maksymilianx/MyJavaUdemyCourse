6f2a93813d2419e110ca012376681e4be49d8d76
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2022-10-09 00:05:34 +0200

    add parsing lesson

--- src/sectionNr5/Lessons/ParsingValues.java
+++ src/sectionNr5/Lessons/ParsingValues.java
@@ -1,2 +1,18 @@
-package sectionNr5.Lessons;public class ParsingValues {
+package sectionNr5.Lessons;
+
+public class ParsingValues {
+
+    public static void main(String[] args) {
+        String numberAsString = "2022";
+        System.out.println("numberAsString = " + numberAsString);
+
+        int number = Integer.parseInt(numberAsString);
+        System.out.println("number = " + number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println("numberAsString = " + numberAsString);
+        System.out.println("number = " + number);
+    }
 }
