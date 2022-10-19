a4844b73b4edb6586fd725ebc9b3381dddb4bd9e
Maksymilian Krajewski/Modem Verification Support II (SDV) /SRPOL/Professional/Samsung Electronics <m.krajewski2@samsung.com>
2022-10-19 15:45:57 +0200

    add finished exercise

--- src/sectionNr5/Exercises/DiagonalStar.java
+++ src/sectionNr5/Exercises/DiagonalStar.java
@@ -5,17 +5,21 @@ public class DiagonalStar {
     public static void printSquareStar(int number) {
         if (number < 5) {
             System.out.println("Invalid Value");
-        }
-        for (int i = 0; i < number; i++) {
-            System.out.print("*");
-            for (i = 0; i < number; i++) {
-                System.out.println("*");
+        } else {
+            for (int row = 0; row < number; row++) {
+                for (int column = 0; column < number; column++) {
+                    if (row == 0 || row == number -1 || column == 0
+                            || column == number -1 || column == row || column == number - 1 - row
+                    )System.out.print("*");
+                    else System.out.print(" ");
+                }
+                System.out.println("");
             }
         }
     }
 
 
     public static void main(String[] args) {
-        printSquareStar(8);
+        printSquareStar(20);
     }
 }
