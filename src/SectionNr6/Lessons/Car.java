9bdeb63d4ee4ff8104d20d4281ab941692af6218
Maksymilian Krajewski/Modem Verification Support II (SDV) /SRPOL/Professional/Samsung Electronics <m.krajewski2@samsung.com>
2022-11-23 20:56:59 +0100

    add lesson with exercise

---
+++ src/SectionNr6/Lessons/Car.java
@@ -0,0 +1,29 @@
+package SectionNr6.Lessons;
+
+import java.util.Locale;
+
+/**
+ *
+ */
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+    public void setModel(String model) {
+
+        String validModel = model.toLowerCase();
+        if (validModel.equals("500") || validModel.equals("laferarri")) {
+            this.model = model;
+        } else {
+            this.model = "Unknown";
+        }
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+}
---
+++ src/SectionNr6/Lessons/Main.java
@@ -0,0 +1,11 @@
+package SectionNr6.Lessons;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car fiat = new Car();
+        Car ferrari = new Car();
+        fiat.setModel("500");
+        System.out.println("Model is " + fiat.getModel());
+    }
+}
