ca3f5107ac9158b61186a753db2142257722859d
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2022-12-18 13:11:43 +0100

    add finished 36th exercise

---
+++ src/SectionNr6/Exercises/ExerciseNr36/Circle.java
@@ -0,0 +1,22 @@
+package SectionNr6.Exercises.ExerciseNr36;
+
+public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        if (radius < 0) {
+            this.radius = 0;
+        } else {
+            this.radius = radius;
+        }
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return (radius*radius*Math.PI);
+    }
+}
