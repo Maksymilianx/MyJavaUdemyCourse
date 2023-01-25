5e5db05fc0dec48dd4e817e0b21e90f71ccaac1a
Maksymilian Krajewski/Modem Verification Support II (SDV) /SRPOL/Professional/Samsung Electronics <m.krajewski2@samsung.com>
2023-01-25 14:56:06 +0100

    add list lesson

---
+++ src/SectionNr10/Lessons/GroceryList.java
@@ -0,0 +1,33 @@
+package SectionNr10.Lessons;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+record GroceryItem(String name, String type, int count) {
+
+    public GroceryItem(String name) {
+        this(name, "DAIRY", 1);
+    }
+}
+
+public class GroceryList {
+
+    public static void main(String[] args) {
+        Object[] groceryArray = new Object[3];
+        groceryArray[0] = new GroceryItem("milk");
+        groceryArray[1] = new GroceryItem("apples", "PRODUCE", 6);
+        groceryArray[2] = new GroceryItem("oranges", "PRODUCE", 5);
+        System.out.println(Arrays.toString(groceryArray));
+
+        ArrayList objectList = new ArrayList();
+        objectList.add(new GroceryItem("Butter"));
+        objectList.add("Yogurt");
+
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
+        groceryList.add(new GroceryItem("Butter"));
+        groceryList.add(new GroceryItem("Yogurt"));
+
+        System.out.println(groceryList);
+
+    }
+}
